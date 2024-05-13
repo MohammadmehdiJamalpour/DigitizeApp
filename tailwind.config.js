0ad413eedfbb2ea0974bfef77f79d866e7c1eff3
Mohammad Mehdi Jamalpour <mohammadmehdi.jamalpour28@gmail.com>
2024-05-13 20:10:50 +0330

    header nav bar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       fontFamily: {
         sans: ["vazir"],
       },
+      gridTemplateColumns: {
+        14: "repeat(14, minmax(0, 1fr))",
+        16: "repeat(16, minmax(0, 1fr))",
+      },
+      gridColumn: {
+        "span-13": "span 13 / span 13", 
+        "span-14": "span 14 / span 14", 
+        "span-15": "span 15 / span 15", 
+        "span-16": "span 16 / span 16", 
+    
+      },
     },
   },
   plugins: [
