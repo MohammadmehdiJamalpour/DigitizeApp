7010a92cf93e3b2362bee3538c65f160d4017b41
Mohammad Mehdi Jamalpour <mohammadmehdi.jamalpour28@gmail.com>
2024-05-16 18:48:33 +0330

    single product html created

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,22 @@ module.exports = {
       fontFamily: {
         sans: ["vazir"],
       },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
       screens: {
-        'xs': '378px',
+        xs: "378px",
+        "435px": "435px",
       },
       gridTemplateColumns: {
         14: "repeat(14, minmax(0, 1fr))",
         16: "repeat(16, minmax(0, 1fr))",
       },
       gridColumn: {
-        "span-13": "span 13 / span 13", 
-        "span-14": "span 14 / span 14", 
-        "span-15": "span 15 / span 15", 
-        "span-16": "span 16 / span 16", 
-    
+        "span-13": "span 13 / span 13",
+        "span-14": "span 14 / span 14",
+        "span-15": "span 15 / span 15",
+        "span-16": "span 16 / span 16",
       },
     },
   },
